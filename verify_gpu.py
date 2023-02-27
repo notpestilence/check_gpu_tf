4beaf0bceeabc7045849849c2900f805b0b5a0ce
Charis Chrisna <62300057+notpestilence@users.noreply.github.com>
2023-02-27 14:38:11 +0700

    updated verify_gpu.py

--- verify_gpu.py
+++ verify_gpu.py
@@ -1,8 +1,8 @@
 from tensorflow.python.client import device_lib
-# Tensorflow 2.10 only
+# Tensorflow 2.10
 def main():
     local_device_protos = device_lib.list_local_devices()
-    return [x.physical_device_desc for x in local_device_protos if x.device_type == 'GPU']
+    print([x.physical_device_desc for x in local_device_protos if x.device_type == 'GPU'])
 
 if __name__ == "__main__":
     main()
